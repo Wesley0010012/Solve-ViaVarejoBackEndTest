fc245ccd68e7e8176e9cd72c910affd4d87e75b9
Wesley0010012 <wglaurindo33@gmail.com>
2023-07-11 20:11:08 -0300

    test: ensure return 400 if invalid product value was provided

--- application/tests/controllers/presentation/parcel-product-controller.spec.ts
+++ application/tests/controllers/presentation/parcel-product-controller.spec.ts
@@ -233,4 +233,31 @@ describe('ParcelProductController Test', () => {
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe(error.message);
   });
+
+  test('Ensure return 400 if invalid product value was provided', () => {
+    const error = new InvalidParamError('product.value');
+
+    const { sut } = makeSut();
+
+    const request: HttpRequest = {
+      body: {
+        product: {
+          code: '1',
+          name: 'any_name',
+          value: 'any_value'
+        },
+        paymentCondiction: {
+          entryValue: '9999',
+          parcelsQuantity: '9999'
+        }
+      }
+    };
+
+    const response: HttpResponse = sut.handle(request);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe(error.message);
+  });
+
+
 });
